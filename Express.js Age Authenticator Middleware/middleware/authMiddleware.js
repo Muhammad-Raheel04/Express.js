a277b6e472a30e9cf30c65e70580338202a9bc4e
Muhammad Raheel <raheel31102004@gmail.com>
2026-01-23 12:31:19 +0500

    added middleware to check user age and provide access on successful verification

--- Express.js Age Authenticator Middleware/middleware/authMiddleware.js
+++ Express.js Age Authenticator Middleware/middleware/authMiddleware.js
@@ -1,15 +1,10 @@
-function sessionAuthenticator(req,res,next){
-    if(req.session && req.session.user){
-        next();
-    }
-    return res.redirect('/');
-}
 function ageAuthenticator(req,res,next){
     if(req.body.age<18){
         return res.redirect('/blocked');
     }
     next();
 }
-module.exports={sessionAuthenticator,ageAuthenticator};
+
+module.exports={ageAuthenticator};
 
 
