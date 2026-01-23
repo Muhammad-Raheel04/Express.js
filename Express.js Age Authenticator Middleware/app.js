1420d12843f07051d4f1ee65f64bae4e0d7cb45c
Muhammad Raheel <raheel31102004@gmail.com>
2026-01-23 12:34:58 +0500

    handled routes separately and used them here to ensure modularity

--- Express.js Age Authenticator Middleware/app.js
+++ Express.js Age Authenticator Middleware/app.js
@@ -5,24 +5,9 @@ const path= require('path')
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 
-function ageAuthenticator(req,res,next){
-    if(req.body.age<18){
-        return res.redirect('/blocked');
-    }
-    res.redirect('/services');
-}
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','pages','index.html'));
-})
+const authRoutes=require('./routes/authRoutes');
 
-app.post('/check-age',ageAuthenticator);
-
-app.get('/blocked',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','pages','denied.html'));
-})
-app.get('/services',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','pages','services.html'));
-})
+app.use('/',authRoutes);
 app.listen(3200,()=>{
     console.log("Server live on http://localhost:3200");
 })
