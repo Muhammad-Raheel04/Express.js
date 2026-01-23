035508fe83fac8cb431069121b5f288b6bacd304
Muhammad Raheel <raheel31102004@gmail.com>
2026-01-23 12:33:38 +0500

    handled routes separately and integrated age authentication middleware to ensure proper validation

---
+++ Express.js Age Authenticator Middleware/routes/authRoutes.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const router =express.Router();
+const path=require('path');
+
+const {ageAuthenticator} =require('../middleware/authMiddleware');
+
+router.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','public','pages','index.html'));
+})
+
+router.post('/check-age',ageAuthenticator,(req,res)=>{
+    res.redirect('/services');
+})
+router.get('/blocked',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','public','pages','denied.html'));
+})
+router.get('/services',ageAuthenticator,(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','public','pages','services.html'));
+})
+module.exports =router;
