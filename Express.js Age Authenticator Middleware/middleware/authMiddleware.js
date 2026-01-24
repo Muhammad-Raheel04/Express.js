7ffdb751f4908b9f6f86137d5aac54677c941604
Muhammad Raheel <raheel31102004@gmail.com>
2026-01-24 21:44:23 +0500

    modified session middleware to display message on the basis of session creation

--- Express.js Age Authenticator Middleware/middleware/authMiddleware.js
+++ Express.js Age Authenticator Middleware/middleware/authMiddleware.js
@@ -1,16 +1,20 @@
-function sessionMiddleware(req,res,next){
-    if(req.session && req.session.Verified){
+function sessionMiddleware(req, res, next) {
+    if (req.session && req.session.Verified) {
         return next();
     }
-    return res.redirect('/');
+    req.session.message = "Please enter your age first.";
+
+    req.session.save(() => {
+        res.redirect('/'); 
+    })
 }
-function ageAuthenticator(req,res,next){
-    if(req.body.age<18){
+function ageAuthenticator(req, res, next) {
+    if (req.body.age < 18) {
         return res.redirect('/blocked');
     }
     next();
 }
 
-module.exports={ageAuthenticator,sessionMiddleware};
+module.exports = { ageAuthenticator, sessionMiddleware };
 
 
