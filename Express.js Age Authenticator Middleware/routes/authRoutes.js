54c1cd46b45af045c38b55d842df9c9dac63a5f2
Muhammad Raheel <raheel31102004@gmail.com>
2026-01-24 18:53:50 +0500

    integrated session authenticator at services route to allow navigation if session exists

--- Express.js Age Authenticator Middleware/routes/authRoutes.js
+++ Express.js Age Authenticator Middleware/routes/authRoutes.js
@@ -1,20 +1,21 @@
-const express=require('express');
-const router =express.Router();
-const path=require('path');
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
-const {ageAuthenticator} =require('../middleware/authMiddleware');
+const { ageAuthenticator, sessionMiddleware } = require('../middleware/authMiddleware');
 
-router.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','public','pages','index.html'));
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'pages', 'index.html'));
 })
 
-router.post('/check-age',ageAuthenticator,(req,res)=>{
+router.post('/check-age', ageAuthenticator, (req, res) => {
+    req.session.Verified = true;
     res.redirect('/services');
 })
-router.get('/blocked',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','public','pages','denied.html'));
+router.get('/blocked', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'pages', 'denied.html'));
 })
-router.get('/services',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','public','pages','services.html'));
+router.get('/services', sessionMiddleware, (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'pages', 'services.html'));
 })
-module.exports =router;
+module.exports = router;
