b75fca97b92136c43cf33a16948a6f717b12919a
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-07 19:21:07 +0500

    integrated express sessions in server

--- form-handler/server.js
+++ form-handler/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express=require('express');
+const session=require('express-session');
 const nodemailer=require('nodemailer');
 const authRoutes=require('./routes/authRoutes');
 const path=require('path');
@@ -9,6 +10,14 @@ app.use(express.static(path.join(__dirname,'public')))
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+app.use(
+    session({
+        secret:process.env.SESSION_SECRET,
+        saveUninitialized:false,
+        resave:false,
+        cookie:{maxAge:30*60*1000}
+    })
+)
 app.use('/',authRoutes);
 
 const PORT=process.env.PORT;
