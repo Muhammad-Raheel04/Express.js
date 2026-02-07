d6f3bc57727595b9110b037f5e5da6608b8c92f6
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-07 21:15:10 +0500

    imported sendForm from form controller and updated post send route

--- form-handler/routes/authRoutes.js
+++ form-handler/routes/authRoutes.js
@@ -2,22 +2,16 @@ const express=require('express');
 const path=require('path');
 const router=express.Router();
 const sessionChecker=require('../middleware/sessionChecker');
+const sendForm=require('../controllers/formController');
+
 router.get('/',(req,res)=>{
     res.status(200).sendFile(path.join(__dirname,'..','public','pages','form.html'));
 })
 router.get('/form',(req,res)=>{
     res.status(302).redirect('/');
 })
-router.post('/send',(req,res)=>{
-    const {name,email,message}=req.body;
-    if(name){
-        req.session.formSubmitted=true;
-        res.status(302).redirect('/success');
-    }
-    else{
-        res.status(500).redirect('/failed');
-    } 
-})
+router.post('/send',sendForm);
+
 router.get('/success',sessionChecker,(req,res)=>{
     res.status(200).sendFile(path.join(__dirname,'..','public','pages','success.html'))
 })
