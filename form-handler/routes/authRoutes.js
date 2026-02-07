d8830d871ccb0883c54211e2d4327ff23a7f9101
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-07 20:41:58 +0500

    imported session middleware created session for user upon send post request and added failed route incase if anything breaks

--- form-handler/routes/authRoutes.js
+++ form-handler/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const path=require('path');
 const router=express.Router();
-
+const sessionChecker=require('../middleware/sessionChecker');
 router.get('/',(req,res)=>{
     res.status(200).sendFile(path.join(__dirname,'..','public','pages','form.html'));
 })
@@ -9,9 +9,19 @@ router.get('/form',(req,res)=>{
     res.status(302).redirect('/');
 })
 router.post('/send',(req,res)=>{
-    res.status(302).redirect('/success');
+    const {name,email,message}=req.body;
+    if(name){
+        req.session.formSubmitted=true;
+        res.status(302).redirect('/success');
+    }
+    else{
+        res.status(500).redirect('/failed');
+    } 
 })
-router.get('/success',(req,res)=>{
+router.get('/success',sessionChecker,(req,res)=>{
     res.status(200).sendFile(path.join(__dirname,'..','public','pages','success.html'))
+})
+router.get('/failed',(req,res)=>{
+    res.status(500).sendFile(path.join(__dirname,'..','public','pages','failed.html'))
 })
 module.exports=router;
